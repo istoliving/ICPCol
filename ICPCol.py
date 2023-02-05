98e3fa06d75312330c5bc7f23c39f4640ed1d4b9
OssianSong <49396268+OssianSong@users.noreply.github.com>
2023-02-05 15:49:01 +0800

    :bell: 对结果输出进行了优化，更新了README文件

--- ICPCol.py
+++ ICPCol.py
@@ -16,7 +16,7 @@ import argparse
 
 def get_user_agent():
     """
-    获取随机UA头
+    获取随机UA头 
     """
     user_agent_list = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1" \
